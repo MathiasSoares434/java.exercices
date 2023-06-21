d8260101d4c0d063f5b434e1bc0a73483c0a2d1b
Mathias Ernandes Oliveira Soares <99788883+MathiasSoares434@users.noreply.github.com>
2023-06-21 16:04:44 -0300

    add more exercices

---
+++ exercicio07.java
@@ -0,0 +1,16 @@
+package Fundamentos;
+
+public class exercicio07 {
+public static void main(String[] args) {
+	int idade = 15;
+	boolean amigoDoDono = true;
+	
+	if(idade < 18 && amigoDoDono == false) {
+		System.out.println("BARRADO");
+	}
+	else {
+		System.out.println("Pode entrar!!!!");
+	}
+			
+}
+}
