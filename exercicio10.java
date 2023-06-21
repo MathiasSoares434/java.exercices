588181332cb166c51e0e1ba989cb8244517fd864
Mathias Ernandes Oliveira Soares <99788883+MathiasSoares434@users.noreply.github.com>
2023-06-21 16:28:34 -0300

    Update exercicio10.java

--- exercicio10.java
+++ exercicio10.java
@@ -5,6 +5,6 @@ public static void main(String[] args) {
 	Scanner ask = new Scanner(System.in);
 	System.out.println("Digite um numero ");
 	double resp = ask.nextDouble();
-	System.out.println(resp < 0 ? "NEGATIVO" : "POSITIVO");
+	System.out.println(resp < 0 ? "NEGATIVO" : resp == 0 ? "NEUTRO" "POSITIVO");
 }
 }
