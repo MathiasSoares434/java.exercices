a8e50f18ddfbe580864432650d2451846d2358c8
Mathias Ernandes Oliveira Soares <99788883+MathiasSoares434@users.noreply.github.com>
2023-06-21 15:47:00 -0300

    adicionando exercícios java.

---
+++ exercicio01.java
@@ -0,0 +1,11 @@
+package Fundamentos;
+
+public class exercicio01 {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 10;
+	
+	System.out.print(a++ + ++a);
+	System.out.print(b++ + b++);
+}
+}
---
+++ exercicio06.java
@@ -0,0 +1,8 @@
+package Fundamentos;
+
+public class exercicio06 {
+public static void main(String[] args) {
+	int a = 3;
+	System.out.println(a % 2 == 0 ? ++a : a++);
+}
+}
